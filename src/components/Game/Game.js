1a9bdb0875e03d8c6185fcbe1240e9b29eba0a85
Emmanuel <endiesworld@gmail.com>
2023-10-17 00:38:37 +0100

    feat: Added the capability to display words on grids

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-import Guess from '../Guess';
+import GuessInput from '../GuessInput';
 import GuessHolder from '../GuessHolder';
 
 // Pick a random word on every pageload.
@@ -14,7 +14,7 @@ function Game() {
   const [guesses, setGuesses] = React.useState([])
   return <>
             <GuessHolder guesses={guesses} />
-            <Guess guesses={guesses} setGuesses={setGuesses}/>
+            <GuessInput guesses={guesses} setGuesses={setGuesses}/>
           </>;
 }
 
